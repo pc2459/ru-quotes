4e86bbf40b6eedeaaeb7003fab45b7f5e09e586c
Po Linn Chia <chiapolinn@gmail.com>
2015-02-03 14:54:47 -0700

    Add star rating CSS

--- main.js
+++ main.js
@@ -1,21 +1,35 @@
 // Create an array of quote objects
 var quotes = [];
 
-// Create a Quote class
-var Quote = function(text, author){
+
+/////////////////
+// Quote class //
+/////////////////
+
+var Quote = function(text, author, rating){
   this.text = text;
   this.author = author;
-  this.ranking = undefined;
+  this.ranking = rating;
 };
 
-// Create a DOM element of a quote
+/**
+ * Create DOM element of a quote
+ */
 Quote.prototype.create = function(){
+  var ratingEl = $('<span>')
+      .addClass("rating")
+      .append('<input type="radio" name="rating" value="1"><i></i>')
+      .append('<input type="radio" name="rating" value="2"><i></i>')
+      .append('<input type="radio" name="rating" value="3"><i></i>')
+      .append('<input type="radio" name="rating" value="4"><i></i>')
+      .append('<input type="radio" name="rating" value="5"><i></i>');
 
-  console.log(this);
   this.$quoteEl = $('<div>')
-      .addClass('quote clearfix')
+      .addClass('quote')
       .append('<p class="quote-text">' + this.text + '</p>')
-      .append('<p class="quote-author">' + this.author + '</p>');
+      .append('<p class="quote-author">' + this.author + '</p>')
+      .append('<span class="quote-delete"><img src="delete.svg"></span>')
+      .append(ratingEl);
   return this.$quoteEl; 
 };
 
@@ -23,31 +37,44 @@ var buildQuoteEls = function(quote){
   return quote.create();
 };
 
-// Add a quote
-var addQuote = function(event){
-  // Prevent the submit button from sending us somewhere cray
-  console.log("Clicked on submit");
+/**
+ * Add a quote upon a click event
+ * @param {click event} event
+ */
+var addQuote = function(event){  
   var newQuote = new Quote($('.quote-input').val(),
-                            $('.author-input').val());
+                          $('.author-input').val());
   quotes.push(newQuote);
-  console.log(quotes);
   loadQuotes();
   return false;
 };
 
-
-
+/**
+ * Empty and reload all quotes into the DOM
+ */
 var loadQuotes = function(){
+  $('.quotes').empty();
   var allQuotes = _.map(quotes, buildQuoteEls);
-  console.log(allQuotes);
-  console.log("GOT HERE");
   $('.quotes').append(allQuotes);
 };
 
+/**
+ * Auto-load quotes into the global quotes array
+ * @param  {array} arr Array of pre-written quotes
+ */
+var autoLoad = function(arr){
+  _.each(arr, function(quote){
+    var newQuote = new Quote(quote.text, quote.author, quote.rating);
+    quotes.push(newQuote);
+  });
+  loadQuotes();
+};
+
+//==========================================
 
 $(document).on('ready', function() {
 
-
+  autoLoad(quoteData);
   
   $('.submit').on('click', addQuote);
 
---
+++ quote-data.js
@@ -0,0 +1,22 @@
+var quoteData = [
+  {
+    text : "Every day I examine myself in three ways: In doing things for others, have I been dishonest? In dealing with friends, have I been untrustworthy? Have I practiced what has been taught to me?",
+    author : "Zeng Zi",
+    rating : ""
+  },
+
+  {
+    text : "It has been my experience that folks who have no vices have very few virtues.",
+    author : "Abraham Lincoln",
+    rating : ""
+  },
+
+  {
+    text : "You have to write the book that wants to be written. And if the book will be too difficult for grown-ups, then you write it for children.",
+    author : "Madeleine L'Engle",
+    rating : ""
+  }
+
+
+
+];
